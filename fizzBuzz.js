f11a0dbfe5cb87aa4be2e94f49ecb25136cb724a
medinak044 <63528703+medinak044@users.noreply.github.com>
2021-10-18 17:04:30 -0500

    FizzBuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,25 @@
+const func1 = () => {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FIZZBUZZ");
+        } else if (i % 3 === 0) {
+            console.log("FIZZ");
+        } else if (i % 5 === 0) {
+            console.log("BUZZ");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+const func2 = () => {
+    let output;
+    for (let i = 0; i < array.length; i++) {
+        if (i % 3 === 0) output += "FIZZ"; // Concatenate string to a variable
+        if (i % 5 === 0) output += "BUZZ";
+
+        if (output === "") output = i; // Check for empty string value
+
+        console.log(output); // Outputs either number or string
+    }
+}
