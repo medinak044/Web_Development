a331ff91a70dc4df32904cb78486063dac82aed8
medinak044 <63528703+medinak044@users.noreply.github.com>
2021-10-22 21:21:06 -0500

    Java practice

--- Java/Practice/Main_01.java
+++ Java/Practice/Main_01.java
@@ -1,5 +1,13 @@
-class Main_01 {
+class Main_01 { // private class for practice only
     public static void main(String[] args) {
-        System.out.println("Hello!");
+
+        int num1 = 5;
+        char character1 = 'A';
+        String firstName = "Kyle";
+        String secondName = "Medina";
+
+        character1 = firstName.charAt(0);
+        System.out.println("Hello " + character1 + secondName.charAt(0) + "!");
+        System.out.printf("Hello %s %s!", firstName, secondName);
     }
 }
