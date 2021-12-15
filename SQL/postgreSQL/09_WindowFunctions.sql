ac5c642aec3d565c1d84c773df5f764e7d1a9b44
medinak044 <63528703+medinak044@users.noreply.github.com>
2021-12-14 18:19:01 -0600

    _

--- SQL/postgreSQL/09_WindowFunctions.sql
+++ SQL/postgreSQL/09_WindowFunctions.sql
@@ -1,4 +1,7 @@
+-- Gets the amount in a particular column
 SELECT first_name, department,
 	COUNT(*) OVER(PARTITION BY department) AS dept_count,
 	COUNT(*) OVER(PARTITION BY region_id) AS region_count
 FROM employees e2
+
+----------------
