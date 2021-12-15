d206485c2983337df72e4db22d1430d8fb7b0d6d
medinak044 <63528703+medinak044@users.noreply.github.com>
2021-12-14 18:39:20 -0600

    _

--- SQL/postgreSQL/09_WindowFunctions.sql
+++ SQL/postgreSQL/09_WindowFunctions.sql
@@ -3,5 +3,13 @@ SELECT first_name, department,
 	COUNT(*) OVER(PARTITION BY department) AS dept_count,
 	COUNT(*) OVER(PARTITION BY region_id) AS region_count
 FROM employees e2
-
 ----------------
+-- Adds up salaries of preceding rows
+SELECT first_name, hire_date, salary,
+	SUM(salary) OVER(ORDER BY hire_date RANGE BETWEEN UNBOUNDED PRECEDING 
+        AND CURRENT ROW) AS running_total_of_salaries
+FROM employees
+-- Same functionality as above query
+SELECT first_name, hire_date, salary,
+	SUM(salary) OVER(PARTITION BY department ORDER BY hire_date) AS running_total_of_salaries
+FROM employees
