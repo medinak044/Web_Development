5532eb9e0739262b7a92bb5d4103e2e1d99bf2f3
medinak044 <63528703+medinak044@users.noreply.github.com>
2021-12-15 19:03:28 -0600

    _

--- SQL/postgreSQL/09_WindowFunctions.sql
+++ SQL/postgreSQL/09_WindowFunctions.sql
@@ -13,3 +13,22 @@ FROM employees
 SELECT first_name, hire_date, salary,
 	SUM(salary) OVER(PARTITION BY department ORDER BY hire_date) AS running_total_of_salaries
 FROM employees
+-- Calculates the salary sum of the current row + the preceding row
+SELECT first_name, hire_date, salary,
+	SUM(salary) OVER(ORDER BY hire_date ROWS BETWEEN 1 PRECEDING 
+					 AND CURRENT ROW)
+FROM employees
+----------------
+-- RANK()
+-- Query below partitions by department and gives a rank based on salary (because salary is being ordered)
+SELECT first_name, hire_date, department, salary,
+	RANK() OVER(PARTITION BY department ORDER BY salary DESC)
+FROM employees
+-- NTILE(5) splits departments into groups of 5 and gives each member of the group a rank number (percentile)
+SELECT first_name, hire_date, department, salary,
+	NTILE(5) OVER(PARTITION BY department ORDER BY salary DESC)
+FROM employees
+-- FIRST_VALUE() displays the first salary in for each department (can be used for comparison)
+SELECT first_name, hire_date, department, salary,
+	FIRST_VALUE(salary) OVER(PARTITION BY department ORDER BY salary DESC)
+FROM employees
