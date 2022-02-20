b7f80482f67459533d36b8d9ad8fc85ded933998
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-02-19 21:02:28 -0600

    Created React app

--- JavaScript/2022_02_16_Practice/scratch.js
+++ JavaScript/2022_02_16_Practice/scratch.js
@@ -1,3 +1,55 @@
+/*
+let numStart = 1;
+let numEnd = 100;
+
+const func1 = () => {
+    for (let i = numStart; i <= numEnd; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FIZZBUZZ");
+        } else if (i % 3 === 0) {
+            console.log("FIZZ");
+        } else if (i % 5 === 0) {
+            console.log("BUZZ");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+const func2 = () => {
+    for (let i = numStart; i <= numEnd; i++) {
+        let output = "";
+        if (i % 3 === 0) output += "FIZZ"; // Concatenate string to a variable
+        if (i % 5 === 0) output += "BUZZ";
+
+        if (output === "") output = i; // Check for empty string value
+
+        console.log(output || i); // Outputs either string or current index number
+    }
+}
+
+(() => {
+    const iterations = 100;
+    let timeValues = [];
+
+    for (let i = 0; i < iterations; i++) {
+        console.time("Time");
+        func1();
+        // Need to extract only string numbers from console.time() (2.24124 ms)
+        timeValues.push(parseFloat(Math.floor(console.timeEnd("Time"))));
+    }
+    let mean = (() => {
+        let sum = 0.0;
+        for (let i = 0; i < timeValues.length; i++) {
+            sum += timeValues[i];
+        }
+        console.log(timeValues);
+        return sum / parseFloat(timeValues.length);
+    })(); // IIFE
+    console.log(`Average time: ${mean}`);
+})(); // IIFE
+*/
+
 // console.log(((num1, num2) => {
 //     let resultStr = ``
 
