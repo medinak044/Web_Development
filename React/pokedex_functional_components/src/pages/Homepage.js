a2154fedf87a76c269ae9e628d570412aa019ada
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-02-27 11:24:53 -0600

    Worked on Homepage

--- React/pokedex_functional_components/src/pages/Homepage.js
+++ React/pokedex_functional_components/src/pages/Homepage.js
@@ -2,23 +2,34 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Row, Col } from 'react-bootstrap'
 
+// Components
+import Pokemon from '../components/Pokemon'
+
 export default function Homepage() {
     const [pokemon, setPokemon] = useState([])
     const [loading, setLoading] = useState(second)
 
-    const getPokemon = async (id) => {
+    const getPokemonList = async (id) => {
         // const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
         // return res
         return await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
     }
 
     useEffect(() => {
-        getPokemon()
+        getPokemonList()
     }, [])
 
     return (
         <div>
-
+            {loading ? (
+                <h1>Loading...</h1>
+            ) : (
+                <Row>{pokemon.map(p => (
+                    <Col key={p.data.name} xs={12} sm={12} md={4} lg={4} xl={4}>
+                        <Pokemon pokemon={p.data}></Pokemon>
+                    </Col>
+                ))}</Row>
+            )}
         </div>
     )
 }
