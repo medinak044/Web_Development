5b6e876fdab24276f42e682cfb94ba2e44264a1c
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-03-24 15:42:04 -0500

    Finished MVP

--- React/03_/src/App.js
+++ React/03_/src/App.js
@@ -1,9 +1,27 @@
+import { useState } from 'react'
 import './App.css';
 
 function App() {
-  return (
-    <div>
+  const [counter, setCounter] = useState(0)
 
+  const handleClickIncrease = () => {
+    setCounter(counter + 1)
+  }
+
+  const handleClickDecrease = () => {
+    setCounter(counter - 1)
+  }
+
+  const handleReset = () => {
+    setCounter(0)
+  }
+
+  return (
+    <div className='App'>
+      <h1>{counter}</h1>
+      <button onClick={handleClickIncrease}>+</button>
+      <button onClick={handleClickDecrease}>-</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
