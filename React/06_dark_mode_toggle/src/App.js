91e5c1e6aa246f8699a597c0d378cdb2111d710c
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-03-27 09:05:45 -0500

    completed dark mode

--- React/06_dark_mode_toggle/src/App.js
+++ React/06_dark_mode_toggle/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import { useState } from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import { lightTheme, darkTheme, GlobalStyles } from './components/styles/themes'
+
+
+const StyledApp = styled.div`
+color: ${props => props.theme.fontColor}
+`
 
 function App() {
-  return (
-    <div className="App">
+  const [theme, setTheme] = useState(`light`)
 
-    </div>
+  const themeToggler = () => {
+    theme === `light` ? setTheme(`dark`) : setTheme(`light`)
+  }
+
+  return (
+    <ThemeProvider theme={theme === `light` ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <StyledApp>Hello
+        <button onClick={themeToggler}>Change theme</button>
+      </StyledApp>
+    </ThemeProvider>
   )
 }
 
