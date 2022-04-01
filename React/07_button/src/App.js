94426383799bbd92abe09256193a2b71cddfdbf7
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-04-01 16:39:21 -0500

    Completed counter logic

--- React/07_button/src/App.js
+++ React/07_button/src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    setCounter(0)
+  }, [])
+
+  const incrementInterval = 1
+  const decrementInterval = 1
+
+  const increment = () => {
+    setCounter(counter + 1)
+  }
+
+  const decrement = () => {
+    setCounter(counter - 1)
+  }
 
   return (
     <div className="App">
-
+      <h1>{counter}</h1>
+      <button onClick={increment}>+{incrementInterval}</button>
+      <button onClick={decrement}>-{decrementInterval}</button>
     </div>
   )
 }
