630efaaffb5b055623bed307bd0a8ee9a4eea9d9
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-04-05 15:48:30 -0500

    All pages scroll back up on next link

--- React/11_basic_website/src/App.js
+++ React/11_basic_website/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import ReactBackground from './components/ReactBackground';
 
 import Header from './components/Header';
+import ReactBackground from './components/ReactBackground';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Profile from './Pages/Profile';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
+
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <div className="App">
         <Header />
       </div >
