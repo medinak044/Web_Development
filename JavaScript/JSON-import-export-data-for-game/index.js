c1eda34ec8cf5ec908f1a1f9adcc8a3e4a650d82
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-04-11 11:45:24 -0500

    Finished json logic

--- JavaScript/JSON-import-export-data-for-game/index.js
+++ JavaScript/JSON-import-export-data-for-game/index.js
@@ -1,7 +1,29 @@
 const fs = require(`fs`)
 
-try {
-    const jsonString = fs.readFileSync(`.customer.json`, `utf-8`)
-    const data = JSON.parse(jsonString)
-    console.log()
-} catch (error) { console.log(err) }
+let filePath = ``
+
+const jsonReader = (filePath, cb) => {
+    if (err) { return cb && cb(err) }
+
+    try {
+        const jsonString = fs.readFileSync(`.customer.json`, `utf-8`)
+        const data = JSON.parse(jsonString)
+        console.log()
+    } catch (error) { console.log(err) }
+}
+
+// jsonReader(`./customer.json`, (err,data)=>{
+//     if (err) { console.log(err) } 
+//     else { console.log(data.address) }
+// })
+
+const newObj = {
+    name: `New name`,
+    address: `456 Example St`
+}
+const jsonString = JSON.stringify(newObj)
+console.log(jsonString)
+fs.writeFile(`./newData.json`, JSON.stringify(newObj, null, 2), err => {
+    if (err) { console.log(err) }
+    else { console.log(`successfully written data`) }
+})
