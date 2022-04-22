ddd07a3f43a3f5305e9dee9183ff525c57dc4032
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-04-21 20:16:41 -0500

    added fake data

--- React/15-todo-app-practice/src/App.js
+++ React/15-todo-app-practice/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const fakeTodoList = [
+    { id: Date.now(), description: 'a', isComplete: false },
+    { id: Date.now(), description: 'b', isComplete: false },
+    { id: Date.now(), description: 'c', isComplete: false },
+  ]
+
+  const [todos, setTodos] = useState([...fakeTodoList])
+  const [newTodo, setNewTodo] = useState('')
+
+  const handleChange = e => { setNewTodo(e.target.value) } // Every time user types, record the change
+
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+  }
+
+  const todoList = todos.map(todo => {
+    return (
+      <div>
+        <li id={todo.id}>{todo.description}</li>
+      </div>
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo list</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" />
+        <button>Submit</button>
+      </form>
+      <div>
+        <ol>
+          {todoList}
+        </ol>
+      </div>
     </div>
   );
 }
