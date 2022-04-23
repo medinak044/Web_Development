5c9585671803324c1b4dc561eadc0bf856dd95da
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-04-22 21:52:40 -0500

    Completed counter

--- React/16-button-counter/src/App.js
+++ React/16-button-counter/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  const increment = () => {
+    setCounter(prev => prev + 1)
+  }
+
+  const decrement = () => {
+    setCounter(prev => prev - 1)
+  }
+
+  useEffect(() => {
+    console.log(counter)
+  }, [counter])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{counter}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
     </div>
   );
 }
