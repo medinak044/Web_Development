a1be21fd851fedae220eedca5d74f86b29108830
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-04-29 19:51:22 -0500

    Added 404

---
+++ React/17-gatsby-project-practice/src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <>
+            <h2>404</h2>
+            <p>Sorry, that page doesn't exist</p>
+        </>
+    )
+}
+
+export default NotFound
---
+++ React/17-gatsby-project-practice/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <>
+            <h1>About Page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, distinctio necessitatibus? Natus porro ducimus illo officiis dicta blanditiis esse incidunt nobis delectus veniam velit deleniti, nostrum minima. Quasi, nulla saepe?</p>
+        </>
+    )
+}
+
+export default About
