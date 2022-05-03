7759d2585342f82e902177a2592e5d475b37e464
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-05-03 14:57:35 -0500

    Set max width for page contents

--- React/17-gatsby-project-practice/src/pages/index.js
+++ React/17-gatsby-project-practice/src/pages/index.js
@@ -13,6 +13,7 @@ export default function Home() {
           <p>UX designer & web developer based in Austin.</p>
           <Link to="/projects" className={styles.btn}>My Portfolio Projects</Link>
         </div>
+        <img src="/banner.png" alt="site banner" style={{ maxWidth: '100%' }} />
       </section>
     </Layout>
   )
