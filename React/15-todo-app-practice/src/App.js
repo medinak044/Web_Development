3023331459b6b66536cf7769a7ad9e3b094e3472
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-05-11 12:44:05 -0500

    Able to add new todos

--- React/15-todo-app-practice/src/App.js
+++ React/15-todo-app-practice/src/App.js
@@ -8,16 +8,36 @@ function App() {
     { id: Date.now(), description: 'c', isComplete: false },
   ]
 
-  const [todos, setTodos] = useState([...fakeTodoList])
-  const [newTodo, setNewTodo] = useState('')
+  const [todos, setTodos] = useState([])
+  const [newTodoDescription, setNewTodoDescription] = useState('')
 
-  const handleChange = e => { setNewTodo(e.target.value) } // Every time user types, record the change
+  const handleChange = e => { setNewTodoDescription(e.target.value) } // Every time user types, record the change
 
 
   const handleSubmit = e => {
     e.preventDefault()
-
+    let newTodo = {
+      id: Date.now(),
+      description: newTodoDescription,
+      isComplete: false
+    }
+    const newTodos = [...todos, newTodo] // Add at end of array
+    setTodos(newTodos)
+    setNewTodoDescription('')
   }
+
+  useEffect(() => {
+    // Retrieve todos from database when page first loads
+    // setTodos([...fakeTodoList])
+    fetch('http://localhost:8000/todos') // Next time use Axios for practice
+      .then(res => {
+        return res.json() // Waits for the data to be retrieved, then parsed
+      })
+      .then(data => {
+        // console.log(data)
+        setTodos(data)
+      })
+  }, [])
 
   const todoList = todos.map(todo => {
     return (
@@ -31,7 +51,7 @@ function App() {
     <div className="App">
       <h1>Todo list</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" />
+        <input onChange={handleChange} type="text" />
         <button>Submit</button>
       </form>
       <div>
